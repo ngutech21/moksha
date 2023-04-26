22916305e830e687fd5d8a576ce094681afc1621
Steffen <99717310+ngutech21@users.noreply.github.com>
2023-04-26 19:40:30 +0200

    chore: fix error handling

--- core/src/error.rs
+++ core/src/error.rs
@@ -1,7 +1,17 @@
+use base64::DecodeError;
 use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum CashuCoreError {
     #[error("Secp256k1Error {0}")]
     Secp256k1Error(#[from] secp256k1::Error),
+
+    #[error("InvalidTokenType")]
+    InvalidTokenPrefix,
+
+    #[error("Base64DecodeError {0}")]
+    Base64DecodeError(#[from] DecodeError),
+
+    #[error("SerdeJsonError {0}")]
+    SerdeJsonError(#[from] serde_json::Error),
 }
