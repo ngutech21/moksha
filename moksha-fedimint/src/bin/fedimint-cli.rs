f5ec309e1798ecf4eff869aaa3f79b4c2343ac1a
ngutech21 <ngutech21@pm.me>
2023-07-24 13:51:56 +0200

    feat: make fedi connection configurable

--- moksha-fedimint/src/bin/fedimint-cli.rs
+++ moksha-fedimint/src/bin/fedimint-cli.rs
@@ -1,23 +1,26 @@
 use moksha_fedimint::FedimintWallet;
+use std::env;
 use std::path::PathBuf;
 use std::str::FromStr;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    dotenvy::dotenv().ok();
+
+    let connection = env::var("CLI_FEDIMINT_CONNECTION")?;
     let workdir = workdir()?;
-    FedimintWallet::connect(workdir.clone(), "fed11sasnnkm0d2exauq87za95te0lshg54mzdd8d039c7ezaf0de0rlvncl8w3sljwde03vcylzyhxurjqqewaen5te0xc6zuv3jxuhrzvpk9ccnxve68qcnwdp03vlw9pz6kfe2p3r2easqhrrjkv").await?;
-    let wallet = FedimintWallet::new(workdir.clone()).await?;
-    if FedimintWallet::is_initialized(&workdir) {
-        println!("Wallet is initialized");
-        let balance = wallet.balance().await?;
-        println!("Balance: {}", balance);
-    } else {
-        println!("Wallet is not initialized");
+    if !FedimintWallet::is_initialized(&workdir) {
+        FedimintWallet::connect(workdir.clone(), &connection).await?;
     }
 
-    // let (operation_id, invoice) = wallet.get_mint_payment_request(1_000).await?;
-    // println!("Invoice: \n{}", invoice);
-    // wallet.mint(operation_id, 1_000).await?;
+    let wallet = FedimintWallet::new(workdir.clone()).await?;
+    println!("Wallet is initialized");
+    let balance = wallet.balance().await?;
+    println!("Balance: {}", balance);
+
+    let (operation_id, invoice) = wallet.get_mint_payment_request(1_000).await?;
+    println!("Invoice: \n{}", invoice);
+    wallet.mint(operation_id).await?;
     Ok(())
 }
 
