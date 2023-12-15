7aa42cbc804d545692d686a29cda9b3225917040
ngutech21 <ngutech21@pm.me>
2023-12-15 16:14:05 +0100

    fix: create /lndconf

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 # Decode the base64 environment variables and write them to files
+mkdir -p /lndconf
 echo "$LND_MACAROON_BASE64" | base64 -d > /lndconf/admin.macaroon
 echo "$LND_TLS_CERT_BASE64" | base64 -d > /lndconf/tls.cert
 
