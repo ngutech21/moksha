0fd9dec5a36d5e5be38e3ca69e6d83ff218ea882
ngutech21 <ngutech21@pm.me>
2024-01-08 13:08:05 +0100

    fix: test

--- moksha-wallet/src/wallet.rs
+++ moksha-wallet/src/wallet.rs
@@ -459,6 +459,7 @@ fn get_blinded_msg(blinded_messages: Vec<(BlindedMessage, SecretKey)>) -> Vec<Bl
 #[cfg(test)]
 mod tests {
     use crate::client::MockClient;
+    use crate::localstore::sqlite::SqliteLocalStore;
     use crate::wallet::WalletBuilder;
     use crate::{
         error::MokshaWalletError,
@@ -652,6 +653,59 @@ mod tests {
 
         let result = wallet.pay_invoice(invoice).await?;
         assert!(result.paid);
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_pay_invoice_can_not_melt() -> anyhow::Result<()> {
+        let fixture = read_fixture("token_64.cashu")?; // 60 tokens (4,8,16,32)
+        let tokens: TokenV3 = fixture.try_into()?;
+
+        let tmp = tempfile::tempdir().expect("Could not create tmp dir for wallet");
+        let tmp_dir = tmp
+            .path()
+            .to_str()
+            .expect("Could not create tmp dir for wallet");
+
+        let localstore = SqliteLocalStore::with_path(format!("{tmp_dir}/test_wallet.db"))
+            .await
+            .expect("Could not create localstore");
+
+        localstore.add_proofs(&tokens.proofs()).await?;
+        assert_eq!(64, localstore.get_proofs().await?.total_amount());
+
+        let melt_response =
+            read_fixture_as::<PostMeltBolt11Response>("post_melt_response_not_paid.json")?;
+
+        let mut mock_client = create_mock();
+        mock_client
+            .expect_post_melt_bolt11()
+            .returning(move |_, _, _, _| Ok(melt_response.clone()));
+
+        let quote_response =
+            read_fixture_as::<PostMeltQuoteBolt11Response>("post_melt_quote_response.json")?;
+        mock_client
+            .expect_post_melt_quote_bolt11()
+            .returning(move |_, _, _| Ok(quote_response.clone()));
+        let swap_response = read_fixture_as::<PostSwapResponse>("post_split_response_24_40.json")?;
+        mock_client
+            .expect_post_swap()
+            .returning(move |_, _, _| Ok(swap_response.clone()));
+
+        let wallet = WalletBuilder::default()
+            .with_client(mock_client)
+            .with_localstore(localstore.clone())
+            .with_mint_url(Url::parse("http://localhost:8080").expect("invalid url"))
+            .build()
+            .await?;
+
+        // 21 sats
+        let invoice = "lnbcrt210n1pjg6mqhpp5pza5wzh0csjjuvfpjpv4zdjmg30vedj9ycv5tyfes9x7dp8axy0sdqqcqzzsxqyz5vqsp5vtxg4c5tw2s2zxxya2a7an0psn9mcfmlqctxzntm3sngnpyk3muq9qyyssqf8z5f90yu3wrmsufnnza25qjlnvc6ukdr094ckzn63ktcy6z5fw5mxf9skndpg2p4648gfjfvvx4qg2lqvlryyycg5k7x9h4dw70t4qq37pegm".to_string();
+
+        let result = wallet.pay_invoice(invoice).await?;
+        assert!(!result.paid);
+        assert_eq!(64, localstore.get_proofs().await?.total_amount());
+        assert!(!result.paid);
         Ok(())
     }
 }
